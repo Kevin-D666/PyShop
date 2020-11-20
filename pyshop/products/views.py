57b7fa45cfd2bc7ef3fc83afd6fff86f9252df53
Kevin-D666 <kdoherty171@gmail.com>
2020-11-20 13:32:32 +0000

    add to index page function

--- pyshop/products/views.py
+++ pyshop/products/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from .models import Product
 # Create your views here.
 
 
 def index(requests):
+    products = Product.objects.all()
+    # Product.objects.filter()
+    # Product.objects.get()
+    # Product.objects.save()
     return HttpResponse('Hello Brian')
 
 
